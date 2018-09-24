0c985ee4659c9a79b0c4153473dd35a876053dd0
Jan Ulferts <julferts@mesosphere.com>
2018-09-24 15:17:47 +0200

    change default format to append the name

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "public-agents" {
   }
 
   https_acm_cert_arn = "${var.https_acm_cert_arn}"
-  elb_name_format    = "%s-public-agents"
+  elb_name_format    = "public-agents-%s"
 
   additional_listener = ["${var.additional_listener}"]
   instances           = ["${var.instances}"]
