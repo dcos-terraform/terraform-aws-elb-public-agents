4e335cc13dba00616fe657c1ec39ba54c7484daa
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2019-03-08 15:27:26 -0800

    output: remove nil output from terraform module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
 output "dns_name" {
   description = "DNS Name of the master load balancer"
-  value       = "${module.public-agents.dns_name}"
+  value       = "${coalesce(module.public-agents.dns_name, "")}"
+
+  # TODO(mbernadin): remove coalesce when nil on destroy is fixed: https://github.com/hashicorp/terraform/issues/17862
 }
